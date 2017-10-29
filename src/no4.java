47f8a2675a84897fc67dff11a21b95bca5131b92
Stickz <vastag.balint@gmail.com>
2017-10-29 17:29:03 +0100

    changed for less memory usage

--- src/no4.java
+++ src/no4.java
@@ -1,25 +1,23 @@
 
 public class no4 {
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 0; i < args.length; i++) {
-            for (int j = i; j < args.length; j++) {
-                if (Integer.parseInt(args[i]) > Integer.parseInt(args[j])) {
-                    String temp = args[j];
-                    args[j] = args[i];
-                    args[i] = temp;
-                }
-            }
-        }
+
+        int max = Integer.parseInt(args[0]);
+        int min = Integer.parseInt(args[0]);
+        float sum = 0;
 
 
         for (int i = 0; i < args.length; i++) {
             sum += Integer.parseInt(args[i]);
+            if (Integer.parseInt(args[i]) > max) {
+                max = Integer.parseInt(args[i]);
+            }
+            if (Integer.parseInt(args[i]) < min) {
+                min = Integer.parseInt(args[i]);
+            }
         }
-
-        int median = sum / args.length;
-        System.out.println(args[args.length - 1]);
-        System.out.println(args[0]);
-        System.out.println(median);
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+        System.out.println("Median: " + sum/args.length);
     }
 }
